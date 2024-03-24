a391766c8cc1d9f2fefcc0a6188524eade9b12aa
sarkarBetaV <126364996+sarkarBetaV@users.noreply.github.com>
2024-03-24 23:41:47 +0600

    my commit

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,28 +1,33 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 
 import Dashboard from "../Layout/Dashboard";
 import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
 import AllEmployess from "../Pages/Dashboard/AllEmployees/AllEmployess";
+import AllSuppliers from "../Pages/Dashboard/AllSuppliers/AllSuppliers";
 
 
-  export const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Dashboard></Dashboard>, 
-      children: [
-        // admin routes
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard></Dashboard>,
+    children: [
+      // admin routes
 
-        {
-          path: 'allUsers', 
-          element: <AllUsers></AllUsers>
-        },
-        {
-          path: 'allEmployees', 
-          element: <AllEmployess/>
-        },
+      {
+        path: 'allUsers',
+        element: <AllUsers></AllUsers>
+      },
+      {
+        path: 'allEmployees',
+        element: <AllEmployess />
+      },
+      {
+        path: 'AllSuppliers',
+        element: <AllSuppliers />
+      },
 
-      ]
-    }
-  ]);
+    ]
+  }
+]);
